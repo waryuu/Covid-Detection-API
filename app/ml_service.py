14210b77f8eb374c7ffbb6591856d5471febebf9
waryuu <01wahyudiputra01@gmail.com>
2026-03-25 13:40:13 +0700

    Add: exception on ml_service

--- app/ml_service.py
+++ app/ml_service.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import librosa
 
 MODEL_PATH = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..", "covid_model.tflite")
+    os.path.join(os.path.dirname(__file__), "model", "covid_model.tflite")
 )
 
 interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
@@ -20,13 +20,18 @@ def preprocess_audio(file_path):
     return np.expand_dims(y, axis=0).astype(np.float32)
 
 def run_inference(file_path):
-    input_data = preprocess_audio(file_path)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.invoke()
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    prediction = output_data[0]
-    label = "positif" if prediction[0] > 0.5 else "negatif"
-    return {
-        "label": label,
-        "confidence": float(prediction[0], 2)
-    }
+    try:
+        input_data = preprocess_audio(file_path)
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        prediction = float(output_data[0])
+        label = "positif" if prediction[0] > 0.5 else "negatif"
+
+        return {
+            "label": label,
+            "confidence": round(prediction[0], 2)
+        }
+    except Exception as e:
+        print(f"Error during inference: {e}")
+        return {"label": "error", "confidence": 0.0}
